0818d211e65c6829b61a324ba297153916a5ed41
Avamshika <aelpulavamshika@gmail.com>
2026-06-07 23:03:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let title = document.getElementById("title");
+let button = document.getElementById("btn");
+button.addEventListener("click", () => {
+  title.innerText = "Welcome to My Developer Journey 🚀";
+  title.style.color = "lime";
+  document.body.style.background = "#111827";
+});
